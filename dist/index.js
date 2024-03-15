b702610a1babf2dfae00a7e9027ea457b8d3ee71
Nicolas Trüssel <nicolas.truessel@ergon.ch>
2024-03-15 19:27:22 +0100

    Filter own Job

--- dist/index.js
+++ dist/index.js
@@ -29017,57 +29017,38 @@ async function run() {
         const octokit = github.getOctokit(core.getInput('token'));
         // 1. Wait for no more checks in progress or requested
         const startTime = new Date();
+        let page = 1;
         // eslint-disable-next-line no-constant-condition
         while (true) {
-            const { data: anyJob } = await octokit.rest.checks.listForRef({
-                ...github.context.repo,
-                per_page: 1,
-                page: 1,
-                ref: github.context.ref
-            });
-            const { data: queued } = await octokit.rest.checks.listForRef({
-                ...github.context.repo,
-                status: 'queued',
-                per_page: 1,
-                page: 1,
-                ref: github.context.ref
-            });
-            const { data: inProgress } = await octokit.rest.checks.listForRef({
-                ...github.context.repo,
-                status: 'in_progress',
-                per_page: 1,
-                page: 1,
-                ref: github.context.ref
-            });
-            if (anyJob.total_count > 0 &&
-                queued.total_count === 0 &&
-                inProgress.total_count === 0) {
-                break;
-            }
+            // Check timeout
             if ((new Date().getTime() - startTime.getTime()) / 1000 / 60 > 10) {
                 // 10 min at most
                 throw new Error('timed out waiting for jobs to finish');
             }
-            await wait(100);
-        }
-        let page = 1;
-        // eslint-disable-next-line no-constant-condition
-        while (true) {
+            // Fetch current page
             const pageSize = 100;
-            const { data: checks } = await octokit.rest.checks.listForRef({
+            const { data: response } = await octokit.rest.checks.listForRef({
                 ...github.context.repo,
                 filter: 'latest',
                 per_page: pageSize,
                 page,
                 ref: github.context.ref
             });
-            page += 1;
-            if (checks.check_runs.some(it => it.conclusion !== 'success')) {
+            const checks = response.check_runs.filter(it => it.name !== github.context.job);
+            // Check if any are incomplete
+            if (checks.some(it => it.status !== 'completed')) {
+                await wait(100);
+                continue;
+            }
+            // Check for errors
+            if (checks.some(it => it.conclusion !== 'success')) {
                 throw new Error('there were unsuccessful checks');
             }
-            if (checks.check_runs.length < pageSize) {
+            // Check if done
+            if (response.check_runs.length < pageSize) {
                 break;
             }
+            page += 1;
         }
     }
     catch (error) {
--- src/main.ts
+++ src/main.ts
@@ -11,62 +11,47 @@ export async function run(): Promise<void> {
 
     // 1. Wait for no more checks in progress or requested
     const startTime = new Date()
+    let page = 1
+
     // eslint-disable-next-line no-constant-condition
     while (true) {
-      const { data: anyJob } = await octokit.rest.checks.listForRef({
-        ...github.context.repo,
-        per_page: 1,
-        page: 1,
-        ref: github.context.ref
-      })
-      const { data: queued } = await octokit.rest.checks.listForRef({
-        ...github.context.repo,
-        status: 'queued',
-        per_page: 1,
-        page: 1,
-        ref: github.context.ref
-      })
-      const { data: inProgress } = await octokit.rest.checks.listForRef({
-        ...github.context.repo,
-        status: 'in_progress',
-        per_page: 1,
-        page: 1,
-        ref: github.context.ref
-      })
-      if (
-        anyJob.total_count > 0 &&
-        queued.total_count === 0 &&
-        inProgress.total_count === 0
-      ) {
-        break
-      }
+      // Check timeout
       if ((new Date().getTime() - startTime.getTime()) / 1000 / 60 > 10) {
         // 10 min at most
         throw new Error('timed out waiting for jobs to finish')
       }
-      await wait(100)
-    }
 
-    let page = 1
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
+      // Fetch current page
       const pageSize = 100
-      const { data: checks } = await octokit.rest.checks.listForRef({
+      const { data: response } = await octokit.rest.checks.listForRef({
         ...github.context.repo,
         filter: 'latest',
         per_page: pageSize,
         page,
         ref: github.context.ref
       })
-      page += 1
 
-      if (checks.check_runs.some(it => it.conclusion !== 'success')) {
+      const checks = response.check_runs.filter(
+        it => it.name !== github.context.job
+      )
+
+      // Check if any are incomplete
+      if (checks.some(it => it.status !== 'completed')) {
+        await wait(100)
+        continue
+      }
+
+      // Check for errors
+      if (checks.some(it => it.conclusion !== 'success')) {
         throw new Error('there were unsuccessful checks')
       }
 
-      if (checks.check_runs.length < pageSize) {
+      // Check if done
+      if (response.check_runs.length < pageSize) {
         break
       }
+
+      page += 1
     }
   } catch (error) {
     // Fail the workflow run if an error occurs
