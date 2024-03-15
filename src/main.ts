f5d80629c32627ef1e6279c3770d3d93753dc445
Nicolas Trüssel <nicolas.truessel@ergon.ch>
2024-03-15 18:56:34 +0100

    Implement Wait-For-Checks

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
 
 /**
  * The main function for the action.
@@ -7,20 +7,75 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const octokit = github.getOctokit(core.getInput('token'))
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // 1. Wait for no more checks in progress or requested
+    const startTime = new Date()
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const { data: anyJob } = await octokit.rest.checks.listForRef({
+        ...github.context.repo,
+        per_page: 1,
+        page: 1,
+        ref: github.context.ref
+      })
+      const { data: queued } = await octokit.rest.checks.listForRef({
+        ...github.context.repo,
+        status: 'queued',
+        per_page: 1,
+        page: 1,
+        ref: github.context.ref
+      })
+      const { data: inProgress } = await octokit.rest.checks.listForRef({
+        ...github.context.repo,
+        status: 'in_progress',
+        per_page: 1,
+        page: 1,
+        ref: github.context.ref
+      })
+      if (
+        anyJob.total_count > 0 &&
+        queued.total_count === 0 &&
+        inProgress.total_count === 0
+      ) {
+        break
+      }
+      if (new Date().getTime() - startTime.getTime() / 1000 / 60 > 10) {
+        // 10 min at most
+        throw new Error('timed out waiting for jobs to finish')
+      }
+      await wait(100)
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let page = 1
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const pageSize = 100
+      const { data: checks } = await octokit.rest.checks.listForRef({
+        ...github.context.repo,
+        filter: 'latest',
+        per_page: pageSize,
+        page,
+        ref: github.context.ref
+      })
+      page += 1
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+      if (checks.check_runs.some(it => it.conclusion !== 'success')) {
+        throw new Error('there were unsuccessful checks')
+      }
+
+      if (checks.check_runs.length < pageSize) {
+        break
+      }
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
+  }
+
+  async function wait(millis: number): Promise<void> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(), millis)
+    })
   }
 }
